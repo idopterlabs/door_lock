00e67dcbdbeee304cb1385535b25c10d8872b36c
Carlos Souza <carloshrsouza@gmail.com>
2025-04-04 15:44:38 -0400

    First stab at state machine

---
+++ lib/door_lock/lock.ex
@@ -0,0 +1,63 @@
+defmodule DoorLock.Lock do
+  @behaviour :gen_statem
+
+  @default_lock_again_timeout 20_000
+
+  def is_locked(pid) do
+    :gen_statem.call(pid, :is_locked)
+  end
+
+  def press_button(pid, code) do
+    :gen_statem.cast(pid, {:press_button, code})
+  end
+
+  ## Callbacks
+
+  def locked(:cast, {:press_button, code}, data) do
+    pressed_buttons = data.pressed_buttons ++ [code]
+
+    if pressed_buttons == data.code do
+      actions = [{:state_timeout, data.lock_again_timeout, :lock}]
+      {:next_state, :unlocked, data, actions}
+    else
+      {:keep_state, %{data | pressed_buttons: pressed_buttons}}
+    end
+  end
+
+  def locked({:call, from}, :is_locked, _data) do
+    {:keep_state_and_data, {:reply, from, true}}
+  end
+
+  def unlocked(:state_timeout, :lock, data) do
+    {:next_state, :locked, %{data | pressed_buttons: []}}
+  end
+
+  def unlocked({:call, from}, :is_locked, _data) do
+    {:keep_state_and_data, {:reply, from, false}}
+  end
+
+  def start_link(opts) do
+    {code, opts} = Keyword.pop(opts, :code)
+
+    {lock_again_timeout, opts} =
+      Keyword.pop(opts, :lock_again_timeout, @default_lock_again_timeout)
+
+    :gen_statem.start_link(__MODULE__, {code, lock_again_timeout}, opts)
+  end
+
+  def init({code, lock_again_timeout}) do
+    {:ok, :locked, %{code: code, lock_again_timeout: lock_again_timeout, pressed_buttons: []}}
+  end
+
+  def callback_mode, do: :state_functions
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+end
---
+++ test/lock_test.exs
@@ -0,0 +1,59 @@
+defmodule DoorLock.LockTest do
+  use ExUnit.Case
+
+  alias DoorLock.Lock
+
+  setup do
+    code = [1, 2, 3]
+    lock_again_timeout = 1_000
+    opts = [code: code, lock_again_timeout: lock_again_timeout]
+    lock_pid = start_supervised!({Lock, opts})
+
+    %{lock_pid: lock_pid, code: code, lock_again_timeout: lock_again_timeout}
+  end
+
+  test "is a process", %{lock_pid: lock_pid} do
+    assert is_pid(lock_pid)
+  end
+
+  test "starts as locked", %{lock_pid: lock_pid} do
+    assert Lock.is_locked(lock_pid)
+  end
+
+  test "unlocks when correct code is pressed", %{lock_pid: lock_pid, code: code} do
+    assert Lock.is_locked(lock_pid)
+
+    for code <- code do
+      assert :ok = Lock.press_button(lock_pid, code)
+    end
+
+    refute Lock.is_locked(lock_pid)
+  end
+
+  test "stays locked when incorrect code is pressed", %{lock_pid: lock_pid, code: code} do
+    assert Lock.is_locked(lock_pid)
+
+    for code <- code do
+      assert :ok = Lock.press_button(lock_pid, code + 1)
+    end
+
+    assert Lock.is_locked(lock_pid)
+  end
+
+  test "locks again after timeout", %{
+    lock_pid: lock_pid,
+    code: code,
+    lock_again_timeout: lock_again_timeout
+  } do
+    assert Lock.is_locked(lock_pid)
+
+    for code <- code do
+      assert :ok = Lock.press_button(lock_pid, code)
+    end
+
+    refute Lock.is_locked(lock_pid)
+
+    Process.sleep(lock_again_timeout + 100)
+    assert Lock.is_locked(lock_pid)
+  end
+end
